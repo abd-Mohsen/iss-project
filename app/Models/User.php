11f67a13fa275e334c8503be43e863f0ecbe2aa7
Abd Moh <abdpuni6pusl9@gmail.com>
2025-01-17 18:31:22 +0300

    nginx works now

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,4 +50,9 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class);
     }
+
+    public function documents()
+    {
+        return $this->hasMany(Document::class);
+    }
 }
